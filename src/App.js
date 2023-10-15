3c31b4e7d7f3c860bbb887551dd8e1209d9d6926
Varun Neelakantan <varun.neelakantan@pwc.com>
2023-10-15 10:44:39 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
-import Cookies from 'universal-cookie';
+import React, { useEffect } from 'react';
+import Cookies from 'js-cookie';
 
-function App() {
+const App = () => {
   useEffect(() => {
-    const cookies = new Cookies();
-    cookies.set("domain2", "HelloDomain2", { path: "/", domain:".3.azurestaticapps.net"});
-    console.log(cookies.get("myCat"));
+    const cookieOptions = {
+      expires: 7,
+      domain: '.azurestaticapps.net', // Replace with your desired domain
+    };
+    Cookies.set('username', 'john_doe', cookieOptions);
+
+    // Get and use the cookie
+    const username = Cookies.get('username');
+    console.log(`Username: ${username}`);
   }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Your component JSX */}
     </div>
   );
-}
+};
 
 export default App;
